636bacdb86cbeaa610ba1c55e37a49a10ee53070
rahulpalamuttam <rahulpalamut@gmail.com>
2015-06-22 09:04:46 -0700

    OpenDap generator

---
+++ src/test/scala/OpenDapURLGenerator$Test.scala
@@ -0,0 +1,14 @@
+import org.dia._
+import org.scalatest.FunSuite
+
+/**
+ * Created by rahulsp on 6/19/15.
+ */
+class OpenDapURLGenerator$Test extends FunSuite {
+
+  test("testDater") {
+    OpenDapURLGenerator.run()
+    assert(true)
+  }
+
+}
