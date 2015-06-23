74cdbf44c0e3795d1cc32537b3c382de9bff6fd1
rahulpalamuttam <rahulpalamut@gmail.com>
2015-06-23 09:40:14 -0700

    bias performance

---
+++ src/test/scala/Main$BreezePerformanceTest.scala
@@ -0,0 +1,37 @@
+//import org.apache.spark.mllib.linalg.DenseMatrix
+import breeze.linalg.DenseMatrix
+import org.dia.Main
+import org.scalatest.FunSuite
+/**
+ * This is a scala breeze implementation of the
+ * metrics test in ocw. The purpose is to
+ * test the performance of simple biasing functions
+ *
+ * Source : https://github.com/apache/climate/blob/master/ocw/metrics.py
+ * Created by rahulsp on 6/22/15.
+ */
+class Main$BreezePerformanceTest extends FunSuite {
+
+  // File URL leader
+  val FILE_LEADER = "http://zipper.jpl.nasa.gov/dist/"
+  // Two Local Model Files
+  val FILE_1 = "AFRICA_KNMI-RACMO2.2b_CTL_ERAINT_MM_50km_1989-2008_tasmax.nc"
+  val FILE_2 = "AFRICA_UC-WRF311_CTL_ERAINT_MM_50km-rg_1989-2008_tasmax.nc"
+  test("ocwMetricsBreezeTest") {
+    val knmi_dataset = Main.getBreezeNetCDFNDVars(FILE_LEADER + FILE_1, "tasmax")
+    val wrf_dataset = Main.getBreezeNetCDFNDVars(FILE_LEADER + FILE_2, "tasmax")
+    val result = new Array[DenseMatrix[Double]](knmi_dataset.length)
+    val totalBefore = System.currentTimeMillis()
+    for(i <- 0 to knmi_dataset.length - 1){
+      val before = System.nanoTime()
+      result(i) = knmi_dataset(i) - wrf_dataset(i)
+      val after = System.nanoTime()
+      println(after - before)
+    }
+
+    val totalAfter = System.currentTimeMillis()
+    println(totalAfter - totalBefore)
+    assert(true)
+  }
+
+}
