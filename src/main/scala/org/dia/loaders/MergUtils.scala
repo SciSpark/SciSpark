ee5a314456a518399ad134f7cdbc557e21b792f1
rahulpalamuttam <rahulpalamut@gmail.com>
2015-08-13 21:18:02 -0700

    merg utils added

--- src/main/scala/org/dia/loaders/MergUtils.scala
+++ src/main/scala/org/dia/loaders/MergUtils.scala
@@ -1,27 +1,8 @@
 package org.dia.loaders
 
-
-import ucar.nc2.dataset.NetcdfDataset
-
-import scala.io.Source
-
+/**
+ * Created by rahulpalamuttam on 8/13/15.
+ */
 object MergUtils {
 
-  def ReadMergtoPair(shape: Array[Int])(offset: Double)(file: String, variable: String): (Array[Double], Array[Int]) = {
-    val k = ReadMergtoJavaArray(file)
-    val halfArray = for (i <- 0 to (k.length / 2)) yield k(i)
-    (halfArray.toArray, shape)
-  }
-
-  def ReadMergtoJavaArray(file: String): Array[Double] = {
-    val Sourcefile = Source.fromFile(file, "ISO8859-1")
-    val byteArray = Sourcefile.map(_.toInt).toArray
-    Sourcefile.close
-    val SourceArray = byteArray.map(floatByte => floatByte.asInstanceOf[Float].toDouble)
-    SourceArray
-  }
-
-  def ReadMergtoNetCDFDataset(file: String, shape: Array[Int]): NetcdfDataset = {
-    null
-  }
 }
