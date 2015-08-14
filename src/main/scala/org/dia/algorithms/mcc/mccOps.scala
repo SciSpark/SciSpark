abd971bea2436fc664a69e64b27130f133d0921d
rahulpalamuttam <rahulpalamut@gmail.com>
2015-08-14 11:45:21 -0700

    change dfs to use stack

--- src/main/scala/org/dia/algorithms/mcc/mccOps.scala
+++ src/main/scala/org/dia/algorithms/mcc/mccOps.scala
@@ -3,6 +3,8 @@ package org.dia.algorithms.mcc
 import org.dia.core.sciTensor
 import org.dia.tensors.AbstractTensor
 
+import scala.collection.mutable
+
 object mccOps {
 
   val BACKGROUND = 0.0
@@ -53,60 +55,12 @@ object mccOps {
     maskedLabels.toList
   }
 
-  def labelConnectedComponents(tensor: AbstractTensor): (AbstractTensor, Int) = {
-    val fourVector = List((1, 0), (-1, 0), (0, 1), (0, -1))
-    val rows = tensor.rows
-    val cols = tensor.cols
-    val labels = tensor.zeros(tensor.shape: _*)
-    var label = 1
-
-    /**
-     * If the coordinates are within bounds,
-     * the input is not 0, and it hasn't been labelled yet
-     * @param row the row to check
-     * @param col the column to check
-     * @return
-     */
-    def isLabeled(row: Int, col: Int): Boolean = {
-      if (row < 0 || col < 0 || row >= rows || col >= cols) return true
-      tensor(row, col) == BACKGROUND || labels(row, col) != BACKGROUND
-    }
-
-    def dfs(row: Int, col: Int, currentLabel: Int): Unit = {
-      if (isLabeled(row, col)) return
-      labels.put(currentLabel, row, col)
-
-      val neighbors = fourVector.map(p => (p._1 + row, p._2 + col))
-      for (neighbor <- neighbors) dfs(neighbor._1, neighbor._2, currentLabel)
-    }
-
-    //First Pass
-    for (row <- 0 to (rows - 1)) {
-      for (col <- 0 to (cols - 1)) {
-        if (!isLabeled(row, col)) {
-          dfs(row, col, label)
-          label += 1
-        }
-      }
-    }
-    (labels, label - 1)
-  }
-
-
-  def findCloudElements(tensor: AbstractTensor): List[AbstractTensor] = {
-    val tuple = labelConnectedComponents(tensor)
-    val labelled = tuple._1
-    val maxVal = tuple._2
-    val maskedLabels = (1 to maxVal).toArray.map(labelled := _.toDouble)
-    maskedLabels.toList
-  }
-
   def findCloudElements(tensor: sciTensor): List[sciTensor] = {
     val labelledTensors = findCloudElements(tensor.tensor)
-    val absT : AbstractTensor = tensor.tensor
+    val absT: AbstractTensor = tensor.tensor
 
     val seq = (0 to labelledTensors.size - 1).map(p => {
-      val masked : AbstractTensor = labelledTensors(p).map(a => if(a != 0.0) 1.0 else a)
+      val masked: AbstractTensor = labelledTensors(p).map(a => if (a != 0.0) 1.0 else a)
 
       val metaTensor = tensor.tensor * masked
       val max = metaTensor.max
@@ -119,9 +73,29 @@ object mccOps {
     seq.toList
   }
 
-  def findCloudElementsX(tensor: AbstractTensor): (AbstractTensor, Int) = {
+  def findCloudElements(tensor: AbstractTensor): List[AbstractTensor] = {
     val tuple = labelConnectedComponents(tensor)
-    tuple
+    val labelled = tuple._1
+    val maxVal = tuple._2
+    val maskedLabels = (1 to maxVal).toArray.map(labelled := _.toDouble)
+    maskedLabels.toList
+  }
+
+  def areaFilled(tensor: AbstractTensor): (Double, Double, Double) = {
+    var count = 0.0
+    var min = Double.MaxValue
+    var max = Double.MinValue
+    val masked = tensor.map(p => {
+      if (p != 0) {
+        if (p < min) min = p
+        if (p > max) max = p
+        count += 1.0
+        1.0
+      } else {
+        p
+      }
+    })
+    (count, max, min)
   }
 
   def findCloudElementsX(tensor: sciTensor): sciTensor = {
@@ -131,21 +105,56 @@ object mccOps {
     new sciTensor(tensor.varInUse, labelledTensor._1, metadata)
   }
 
-    def areaFilled(tensor: AbstractTensor): (Double, Double, Double) = {
-      var count = 0.0
-      var min = Double.MaxValue
-      var max = Double.MinValue
-      val masked = tensor.map(p => {
-        if (p != 0) {
-          if (p < min) min = p
-          if (p > max) max = p
-          count += 1.0
-          1.0
-        } else {
-          p
-        }
-      })
-      (count, max, min)
+  def findCloudElementsX(tensor: AbstractTensor): (AbstractTensor, Int) = {
+    val tuple = labelConnectedComponents(tensor)
+    tuple
+  }
+
+  def labelConnectedComponents(tensor: AbstractTensor): (AbstractTensor, Int) = {
+    val fourVector = List((1, 0), (-1, 0), (0, 1), (0, -1))
+    val rows = tensor.rows
+    val cols = tensor.cols
+    val labels = tensor.zeros(tensor.shape: _*)
+    var label = 1
+    val stack = new mutable.Stack[(Int, Int)]()
+    /**
+     * If the coordinates are within bounds,
+     * the input is not 0, and it hasn't been labelled yet
+     * @param row the row to check
+     * @param col the column to check
+     * @return
+     */
+    def isLabeled(row: Int, col: Int): Boolean = {
+      if (row < 0 || col < 0 || row >= rows || col >= cols) return true
+      tensor(row, col) == BACKGROUND || labels(row, col) != BACKGROUND
     }
+
+
+
+    def dfs(currentLabel: Int): Unit = {
+      while (!stack.isEmpty) {
+        val tuple = stack.pop
+        val row = tuple._1
+        val col = tuple._2
+        labels.put(currentLabel, row, col)
+        val neighbors = fourVector.map(p => (p._1 + row, p._2 + col))
+        for (neighbor <- neighbors) {
+          if (!isLabeled(neighbor._1, neighbor._2)) stack.push((neighbor._1, neighbor._2))
+        }
+      }
+    }
+
+    //First Pass
+    for (row <- 0 to (rows - 1)) {
+      for (col <- 0 to (cols - 1)) {
+        if (!isLabeled(row, col)) {
+          stack.push((row, col))
+          label += 1
+          dfs(label)
+        }
+      }
+    }
+    (labels, label - 1)
+  }
 
 }
