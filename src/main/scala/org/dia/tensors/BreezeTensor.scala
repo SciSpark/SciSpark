35f1df38d1bc011db8fb06831b82218c39606a76
rahulpalamuttam <rahulpalamut@gmail.com>
2015-08-19 10:51:10 -0700

    revert back to operations that create new objects

--- src/main/scala/org/dia/tensors/BreezeTensor.scala
+++ src/main/scala/org/dia/tensors/BreezeTensor.scala
@@ -52,15 +52,15 @@ class BreezeTensor(val tensor: DenseMatrix[Double]) extends AbstractTensor {
   def map(f : Double => Double) : BreezeTensor = tensor.map(p => f(p))
   def put(value: Double, shape: Int*): Unit = tensor.update(shape(0), shape(1), value)
 
-  def +(array: BreezeTensor): BreezeTensor = tensor += array.tensor
+  def +(array: BreezeTensor): BreezeTensor = tensor + array.tensor
 
-  def -(array: BreezeTensor): BreezeTensor = tensor -= array.tensor
+  def -(array: BreezeTensor): BreezeTensor = tensor - array.tensor
 
   def \(array: BreezeTensor): BreezeTensor = tensor \ array.tensor
 
-  def /(array: BreezeTensor): BreezeTensor = tensor /= array.tensor
+  def /(array: BreezeTensor): BreezeTensor = tensor / array.tensor
 
-  def *(array: AbstractTensor): BreezeTensor = tensor :*= array.tensor
+  def *(array: AbstractTensor): BreezeTensor = tensor :* array.tensor
 
   def <=(num: Double): BreezeTensor = tensor.map(v => if (v <= num) v else 0.0)
 
@@ -69,7 +69,7 @@ class BreezeTensor(val tensor: DenseMatrix[Double]) extends AbstractTensor {
   /**
    * Linear Algebra Operations
    */
-  def **(array: BreezeTensor): BreezeTensor = tensor *= array.tensor
+  def **(array: BreezeTensor): BreezeTensor = tensor * array.tensor
 
   def data: Array[Double] = tensor.t.toArray
 
