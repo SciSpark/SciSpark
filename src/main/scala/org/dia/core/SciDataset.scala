0154674a6b3ea82770bbd7d7c9651f4a8c011390
rpalamut <rahulpalamut@gmail.com>
2016-08-12 21:52:23 -0700

    SciDataset write to Netcdf files, Variables record dimension values

--- src/main/scala/org/dia/core/SciDataset.scala
+++ src/main/scala/org/dia/core/SciDataset.scala
@@ -17,11 +17,13 @@
  */
 package org.dia.core
 
+import java.util
+
 import scala.collection.JavaConverters._
 import scala.collection.mutable
 
-import ucar.nc2.Attribute
-import ucar.nc2.dataset
+import ucar.ma2.{Array, DataType}
+import ucar.nc2.{dataset, Attribute, NetcdfFileWriter}
 
 import org.dia.utils.NetCDFUtils
 
@@ -29,26 +31,47 @@ import org.dia.utils.NetCDFUtils
  * Dataset is a logical container for variable arrays.
  * A NetcdfDataset wraps a LinkedHashMap of variables and
  * a LinkedHashMap of attributes.
+ *
+ * The LinkedHashMap preserves insertion order for iteration purposes.
  */
 class SciDataset(val variables: mutable.LinkedHashMap[String, Variable],
-                 val attributes: mutable.LinkedHashMap[String, String]) extends Serializable{
+                 val attributes: mutable.LinkedHashMap[String, String],
+                 val datasetName: String) extends Serializable{
 
-  def this(vars : Traversable[(String, Variable)], attr : Traversable[(String, String)]) {
+  def this(vars : Traversable[(String, Variable)], attr : Traversable[(String, String)], datasetName : String) {
     this(new mutable.LinkedHashMap[String, Variable] ++= vars,
-         new mutable.LinkedHashMap[String, String] ++= attr)
+         new mutable.LinkedHashMap[String, String] ++= attr,
+         datasetName)
   }
 
-  def this(vars : Iterable[ucar.nc2.Variable], attr : Iterable[Attribute]) {
-    this(vars.map(p => (p.getFullName, new Variable(p))), attr.map(p => NetCDFUtils.convertAttribute(p)))
+  def this(vars : Iterable[ucar.nc2.Variable], attr : Iterable[Attribute], datasetName : String) {
+    this(vars.map(p => (p.getFullName, new Variable(p))),
+         attr.map(p => NetCDFUtils.convertAttribute(p)),
+         datasetName)
   }
 
   def this(nvar: dataset.NetcdfDataset) {
-    this(nvar.getVariables.asScala, nvar.getGlobalAttributes.asScala)
+    this(nvar.getVariables.asScala, nvar.getGlobalAttributes.asScala, nvar.getLocation.split("/").last)
   }
 
   def this(nvar: dataset.NetcdfDataset, vars: List[String]) {
-    this(vars.map(vr => nvar.findVariable(vr)), nvar.getGlobalAttributes.asScala)
+    this(vars.map(vr => nvar.findVariable(vr)), nvar.getGlobalAttributes.asScala, nvar.getLocation.split("/").last)
   }
+
+  /**
+   * Extract all the dimension names from the variables
+   * Convert them into a string representation e.g.
+   *
+   * row(400), cols(1440)
+   *
+   * Flatten all the dimensions amongst the variables
+   * find the distinct dimensions
+   */
+  val globalDimensions = variables.valuesIterator.map(variable =>
+      variable.dims.map({
+        case(dimName, length) => dimName + "(" + length + ")"
+      })
+  ).flatten.toList.distinct
 
   /**
    * Writes attribute in the form of key-value pairs
@@ -96,28 +119,55 @@ class SciDataset(val variables: mutable.LinkedHashMap[String, Variable],
    */
   def attr(key: String): String = attributes(key)
 
+  def write(name: String = datasetName): Unit = {
+
+    val writer = NetcdfFileWriter.createNew(NetcdfFileWriter.Version.netcdf3, name, null)
+    val netcdfKeyValue = variables.map {
+      case (key, variable) =>
+        val dims = new util.ArrayList[ucar.nc2.Dimension]()
+        for ((dimName, length) <- variable.dims) {
+          val newDim = writer.addDimension(null, dimName, length)
+          dims.add(newDim)
+        }
+        val varT = writer.addVariable(null, key, ucar.ma2.DataType.FLOAT, dims)
+        varT.addAll(variable.attributes.map(p => new Attribute(p._1, p._2)).asJava)
+        val dataOut = Array.factory(DataType.DOUBLE, variable.shape(), variable.data())
+        (varT, dataOut)
+    }
+    for ((key, attribute) <- attributes) {
+      writer.addGroupAttribute(null, new Attribute(key, attribute))
+    }
+
+    writer.create()
+    for ((variable, array) <- netcdfKeyValue) writer.write(variable, array)
+    writer.close()
+  }
+
   /**
    * Creates a clone of the variable
    *
    * @return
    */
-  def copy(): SciDataset = new SciDataset(variables.clone(), attributes.clone())
+  def copy(): SciDataset = new SciDataset(variables.clone(), attributes.clone(), datasetName)
 
   override def toString: String = {
-    val header = "root group ...\n"
-    val variableString = variables.map({case (str, varb) => varb.dataType + " " + str})
+    val header = datasetName + "\nroot group ...\n"
+    val dimensionString = "\tdimensions(sizes): " + globalDimensions.toString + "\n"
+    val variableString = variables.map({case (str, varb) => varb.dataType + " " + str + varb.dims.keys.toList})
     val footer = "\tvariables: " + variableString + "\n"
     val body = new StringBuilder()
     body.append(header)
     for ((k, v) <- attributes) {
       body.append("\t" + k + ": " + v + "\n")
     }
-    body.append(footer.replaceAll("(ArrayBuffer|\\(|\\))", ""))
+    body.append(dimensionString.replace("List", ""))
+    body.append(footer.replaceAll("(ArrayBuffer|List)", ""))
     body.toString()
   }
 
   override def equals(any: Any): Boolean = {
     val dataset = any.asInstanceOf[SciDataset]
+    dataset.datasetName == this.datasetName &&
     dataset.attributes == this.attributes &&
     dataset.variables == this.variables
   }
